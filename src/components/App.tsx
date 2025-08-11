4a6b55553acce7425eeeca2b512c292779afb370
Akash Sharma <sharma.54akash@gmail.com>
2025-08-11 14:24:59 +0530

    tailwind added

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-gray-100 dark:bg-gray-900 min-h-screen flex flex-col items-center justify-center'>
+        <h1 className="text-3xl font-bold underline mb-4">Welcome</h1>
+        <button className="px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">Tailwind Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
